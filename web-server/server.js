4b03d42e437c64cd18cc27c7157be6f7c12b84a1
Daniele Agueci <d.agueci1@gmail.com>
2018-06-07 15:17:22 -0400

    Add handlebars templating

--- web-server/server.js
+++ web-server/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const port = '3000';
 
 let app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', str => str.toUpperCase());
+
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Daniele',
-        likes: [
-            'sports',
-            'board games'
-        ]
-    });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page!'
+    })
 });
 
 app.get('/about', (req, res) => {
-    res.send('About');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
