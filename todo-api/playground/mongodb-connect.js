4a77efc8ad304e11cd9519877e993e067b94b05a
Daniele Agueci <d.agueci1@gmail.com>
2018-06-08 20:35:29 -0400

    Add sample app changes

--- todo-api/playground/mongodb-connect.js
+++ todo-api/playground/mongodb-connect.js
@@ -10,12 +10,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').find().toArray().then(docs => {
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2));
-    }).catch(err => {
-        console.log('Unable to fetch todos:', err);
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         console.log('Unabled to add todo', err);
+    //         return;
+    //     }
+
+    //     console.log('Added todo document:', JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    let name = 'Jon';
+    let age = 30;
+    let location = 'The Wall';
+    let user = { name, age, location };
+
+    db.collection('Users').insertOne(user, (err, result) => {
+        if (err) {
+            console.log('Unabled to add user', err);
+            return;
+        }
+
+        console.log(result.ops[0]._id.getTimestamp());
     });
 
-    //client.close();
+    client.close();
 });
