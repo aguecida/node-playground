0f9a748c5f52e02b812c03bd612b938a6bb9aa19
Daniele Agueci <d.agueci1@gmail.com>
2018-06-10 22:22:50 -0400

    Add environment envs

--- todo-api/server/db/mongoose.js
+++ todo-api/server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp').then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to MongoDB server');
 }).catch(err => {
     console.log('Unable to connect to MongoDB server');
