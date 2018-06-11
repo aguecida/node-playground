51fe048c9ae0627d6be9dd9f8a8d2106a90bc17a
Daniele Agueci <d.agueci1@gmail.com>
2018-06-11 19:56:55 -0400

    Add delete token api and test

--- todo-api/server/tests/server.test.js
+++ todo-api/server/tests/server.test.js
@@ -239,7 +239,7 @@ describe('POST /users/login', () => {
                 expect(res.headers['x-auth']).toBeTruthy();
             })
             .end((err, res) => {
-                if (err) done(err);
+                if (err) return done(err);
 
                 User.findById(users[1]._id).then(user => {
                     expect(user.tokens[0]).toMatchObject({
@@ -264,5 +264,22 @@ describe('POST /users/login', () => {
                 expect(res.headers['x-auth']).toBeFalsy();
             })
             .end(done);
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+
+                User.findById(users[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(err => done(err));
+            });
     });
 });
