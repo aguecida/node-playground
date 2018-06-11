8528fb528d16121cda4db3bdff7a257d7caa9ef8
Daniele Agueci <d.agueci1@gmail.com>
2018-06-11 15:18:35 -0400

    Hash user password before saving to db

--- todo-api/playground/hashing.js
+++ todo-api/playground/hashing.js
@@ -1,15 +1,34 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+let password = 'abc123';
 
-let token = jwt.sign(data, 'abc123');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-let decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+let hashedPassword = '$2a$10$URwHURUZ6sSRK3Zd0aV7SeIjZybg71IQ3Xaq8.zkKpVwZygdiT47y';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// ------------------
+
+// let data = {
+//     id: 10
+// };
+
+// let token = jwt.sign(data, 'abc123');
+// console.log(token);
+
+// let decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
+
+// -------------------
 
 // let message = 'I am user number 3';
 // let hash = SHA256(message).toString();
