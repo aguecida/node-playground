9819bdc6104f96fa4f2275bd7b66c8de9624ed5b
Daniele Agueci <d.agueci1@gmail.com>
2018-06-12 14:41:57 -0400

    Add UI for sending messages

--- chat-app/public/js/index.js
+++ chat-app/public/js/index.js
@@ -1,13 +1,25 @@
-let socket = io();
+$(document).ready(() => {
 
-socket.on('connect', () => {
-    console.log('Connected to server');
-});
+    let socket = io();
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-});
+    socket.on('connect', () => {
+        console.log('Connected to server');
+    });
 
-socket.on('newMessage', data => {
-    console.log('New message received:', data);
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+    });
+
+    socket.on('newMessage', data => {
+        let li = $('<li></li>').text(`${data.from}: ${data.text}`);
+        $('#messages').append(li);
+    });
+
+    $('#message-form').on('submit', e => {
+        e.preventDefault();
+        
+        socket.emit('createMessage', { from: 'User', text: $(e.target).find('input').val() }, () => {
+            $(e.target).find('input').val('');
+        });
+    });
 });
--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -20,10 +20,11 @@ io.on('connection', socket => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined the chat room'));
 
-    socket.on('createMessage', message => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage:', message);
 
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('123');
         //socket.broadcast.emit('newMessage', { from: message.from, text: message.text, createdAt: new Date().getTime() });
     });
 
